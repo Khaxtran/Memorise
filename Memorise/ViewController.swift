8260540be8386637d976a54a913539adae1e0b99
Kha Tran <khatran961217@gmail.com>
2022-04-19 10:33:21 +0700

    Initial Commit

---
+++ Memorise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memorise
+//
+//  Created by Kha Tran on 19/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
